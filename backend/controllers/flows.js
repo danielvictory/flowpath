a4135ed85f9f9dbc5520e0b26ae7836f07c80cbf
danielvictory <danieljvictory@gmail.com>
2023-05-22 09:38:18 -0500

    add delete and update routes to the flows controller

--- backend/controllers/flows.js
+++ backend/controllers/flows.js
@@ -20,6 +20,24 @@ flowRouter.get('/', async(req, res) => {
     }
 })
 
+// ===== Delete
+flowRouter.delete('/:id', async (req, res) => {
+    try {
+        res.json(await Flow.findByIdAndRemove(req.params.id));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+// ===== Update
+flowRouter.put('/:id', async (req, res) => {
+    try {
+        res.json( await Flow.findByIdAndUpdate(req.params.id, req.body, {new: true}));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
 // ===== Create
 flowRouter.post('/', async (req, res) => {
     console.log('trying to post')
@@ -28,7 +46,7 @@ flowRouter.post('/', async (req, res) => {
     } catch (error) {
         res.status(400).json(error);
     }
-})
+});
 
 // Export controller
 module.exports = flowRouter;
